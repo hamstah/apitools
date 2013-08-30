ff27ecfe82b4b6354388b3042b97883f0fe21014
Nicolas Esteves <hamstahguru@gmail.com>
2013-08-30 16:30:15 +0100

    Removed requirements for the whole package

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 from setuptools import setup
-import os
-
-with open(os.path.join(os.path.dirname(__file__),"requirements.txt")) as req_file:
-    requirements = req_file.read().splitlines()
 
 setup(
     name='apitools',
@@ -13,5 +9,4 @@ setup(
     url='https://github.com/hamstah/apitools',
     description='Tools to play with json-schema and rest apis',
     long_description=open("README.md").read(),
-    install_requires=requirements,
 )
